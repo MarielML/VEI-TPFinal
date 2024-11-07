696bca423bfea223e0c9ef16122ab01a149a86f1
Mariel <marielmarmol1511@outlook.com>
2024-11-07 20:07:25 -0300

    inicio

---
+++ js/cerrarSesion.js
@@ -0,0 +1,15 @@
+const profileImg = document.getElementById("profile-img");
+const logoutBtn = document.getElementById("logout-btn");
+
+profileImg.addEventListener("click", () => {
+  if (logoutBtn.style.display === "none" || logoutBtn.style.display === "") {
+    logoutBtn.style.display = "block";
+  } else {
+    logoutBtn.style.display = "none";
+  }
+});
+
+logoutBtn.addEventListener("click", () => {
+  window.location.href = "../index.html";
+  logoutBtn.style.display = "none";
+});
