56c7ceda37b08bfd92ba182fc84c8d3b3963bd6c
Mariel <marielmarmol1511@outlook.com>
2024-11-11 17:21:16 -0300

    guardados por materia

--- js/data.js
+++ js/data.js
@@ -3,15 +3,21 @@ const LOCAL_STORAGE_GUARDADOS = "guardados";
 const DISENIO_EMOCIONAL = {
   nombre: "Diseño emocional",
   materia: "Visualización e Interfaces",
-  href: "../pdf/Diseño emocional.pdf"
-}
+  href: "../pdf/Diseño emocional.pdf",
+};
 
+const LENGUAJE_PHP = {
+  nombre: "Lenguaje PHP",
+  materia: "Programación Web II",
+  href: "../pdf/Lenguaje PHP.pdf",
+};
 
-const GUARDADOS = [
-  DISENIO_EMOCIONAL
-];
-      
-localStorage.setItem(
-  LOCAL_STORAGE_GUARDADOS,
-  JSON.stringify(GUARDADOS)
-);
+const VISUALIZACION_DE_DATOS = {
+  nombre: "Visualización de datos",
+  materia: "Visualización e Interfaces",
+  href: "../pdf/Visualización de datos.pdf",
+};
+
+const GUARDADOS = [DISENIO_EMOCIONAL, LENGUAJE_PHP, VISUALIZACION_DE_DATOS];
+
+localStorage.setItem(LOCAL_STORAGE_GUARDADOS, JSON.stringify(GUARDADOS));
--- js/guardados.js
+++ js/guardados.js
@@ -1,1 +1,47 @@
-agregarGuardado();
+let nodo_section = document.querySelector("#guardados");
+const JSON_GUARDADOS = localStorage.getItem(LOCAL_STORAGE_GUARDADOS);
+const CONTENIDOS_GUARDADOS = JSON.parse(JSON_GUARDADOS) || [];
+
+function agruparPorMateria() {
+  const agrupadosPorMateria = {};
+
+  CONTENIDOS_GUARDADOS.forEach((i) => {
+    if (agrupadosPorMateria[i.materia]) {
+      agrupadosPorMateria[i.materia].push(i);
+    } else {
+      agrupadosPorMateria[i.materia] = [i];
+    }
+  });
+
+  return agrupadosPorMateria;
+}
+
+function crearArticlesAgrupados() {
+  const AGRUPADOS = agruparPorMateria();
+
+  Object.keys(AGRUPADOS).forEach((materia) => {
+    let nodo_div_materia = document.createElement("div");
+
+    let nodo_h2 = document.createElement("h2");
+    nodo_h2.textContent = materia;
+    nodo_div_materia.appendChild(nodo_h2);
+
+    let nodo_ul = document.createElement("ul");
+
+    AGRUPADOS[materia].forEach((i) => {
+      let nodo_li = document.createElement("li");
+      let nodo_a = document.createElement("a");
+      nodo_a.href = i.href;
+      nodo_a.textContent = i.nombre;
+      nodo_a.target = "_blank";
+      nodo_li.appendChild(nodo_a);
+      nodo_ul.appendChild(nodo_li);
+    });
+
+    nodo_div_materia.appendChild(nodo_ul);
+
+    nodo_section.appendChild(nodo_div_materia);
+  });
+}
+
+crearArticlesAgrupados();
