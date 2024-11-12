611c9d4dcf902b46d36ff0113746bf02cf78ad99
Mariel <marielmarmol1511@outlook.com>
2024-11-12 20:01:28 -0300

    saludo y contenidos

--- js/ingresar.js
+++ js/ingresar.js
@@ -3,7 +3,24 @@ const JSON_GUARDADOS = localStorage.getItem(LOCAL_STORAGE_GUARDADOS);
 
 const CONTENIDOS_GUARDADOS = JSON.parse(JSON_GUARDADOS) || [];
 
-INGRESAR.addEventListener("click", () => {
+function actualizarSaludo() {
+  const horaActual = new Date().getHours();
+  let saludo = "";
+
+  if (horaActual >= 5 && horaActual < 12) {
+    saludo = "¡Buenos días!";
+  } else if (horaActual >= 12 && horaActual < 20) {
+    saludo = "¡Buenas tardes!";
+  } else {
+    saludo = "¡Buenas noches!";
+  }
+
+  document.getElementById("saludo").textContent = saludo;
+}
+
+window.onload = actualizarSaludo;
+
+INGRESAR.addEventListener("click", (event) => {
   event.preventDefault();
   if (CONTENIDOS_GUARDADOS.length === 0) {
     window.location.href = "./paginas/home.html";
