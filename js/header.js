4a72d911cc12e6a1522d2deac8ea40ab657a1b1a
Mariel <marielmarmol1511@outlook.com>
2024-11-13 17:21:30 -0300

    header

--- js/header.js
+++ js/header.js
@@ -1,6 +1,12 @@
 const hamburgerBtn = document.getElementById("hamburger-btn");
 const navItems = document.getElementById("nav-items");
+const foto = document.getElementById("profile-img");
 
 hamburgerBtn.addEventListener("click", () => {
   navItems.classList.toggle("active");
+  if (foto.style.display === "none") {
+    foto.style.display = "block";
+  } else {
+    foto.style.display = "none";
+  }
 });
