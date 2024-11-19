d76e83061858ab4b8c08ad5caba24a060f1342d5
Mariel <marielmarmol1511@outlook.com>
2024-11-19 17:31:17 -0300

    notas

--- js/data.js
+++ js/data.js
@@ -5,7 +5,7 @@ const DISENIO_EMOCIONAL = {
   nombre: "Diseño emocional.pdf (1 MB)",
   materia: "Visualización e Interfaces",
   href: "../pdf/Diseño emocional.pdf",
-  extension: "../imagenes/pdf.jpg"
+  extension: "../imagenes/pdf.jpg",
 };
 
 const LENGUAJE_PHP = {
@@ -13,7 +13,15 @@ const LENGUAJE_PHP = {
   nombre: "Lenguaje PHP.pdf (701 KB)",
   materia: "Programación Web II",
   href: "../pdf/Lenguaje PHP.pdf",
-  extension: "../imagenes/pdf.jpg"
+  extension: "../imagenes/pdf.jpg",
+};
+
+const PHP_EJERCICIO1 = {
+  cuatrimestre: "Segundo cuatrimestre 2024",
+  nombre: "PHP ejercicio 1.txt (15 B)",
+  materia: "Programación Web II",
+  href: "../pdf/PHP ejercicio 1.txt",
+  extension: "../imagenes/txt.jpg",
 };
 
 const VISUALIZACION_DE_DATOS = {
@@ -21,7 +29,7 @@ const VISUALIZACION_DE_DATOS = {
   nombre: "Visualización de datos.pdf (370 KB)",
   materia: "Visualización e Interfaces",
   href: "../pdf/Visualización de datos.pdf",
-  extension: "../imagenes/pdf.jpg"
+  extension: "../imagenes/pdf.jpg",
 };
 
 const MIRO_LEO_PIENSO = {
@@ -29,9 +37,15 @@ const MIRO_LEO_PIENSO = {
   nombre: "Práctica Miro, Leo y Pienso.docx (11.3 KB)",
   materia: "Visualización e Interfaces",
   href: "../pdf/Practica-Miro.docx",
-  extension: "../imagenes/word.jpg"
-}
+  extension: "../imagenes/word.jpg",
+};
 
-const GUARDADOS = [LENGUAJE_PHP, DISENIO_EMOCIONAL, VISUALIZACION_DE_DATOS, MIRO_LEO_PIENSO];
+const GUARDADOS = [
+  LENGUAJE_PHP,
+  PHP_EJERCICIO1,
+  DISENIO_EMOCIONAL,
+  VISUALIZACION_DE_DATOS,
+  MIRO_LEO_PIENSO,
+];
 
 localStorage.setItem(LOCAL_STORAGE_GUARDADOS, JSON.stringify(GUARDADOS));
